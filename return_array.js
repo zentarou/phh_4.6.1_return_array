3811a6057be2af8b9456616322edb2f6e99256c6
zentarou sasaki <zentarou_17n1100200@nnn.ed.jp>
2017-06-28 16:39:12 +0900

    教科書の内容を写経

---
+++ return_array.js
@@ -0,0 +1,10 @@
+function getMaxMin(...nums) {
+    return [Math.max(...nums), Math.min(...nums)];
+}
+
+let result = getMaxMin(10, 35, -5, 78, 0);
+console.log(result);
+
+let[max, min] = getMaxMin(10, 35, -5, 78, 0);
+console.log(max);
+console.log(min);
